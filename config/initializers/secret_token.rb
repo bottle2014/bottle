4a076f939925560581876246b670a8bb267d0b32
Bottle User2014 <bottle2014@gmail.com>
2011-05-27 08:00:05 -0400

    first commitgit add .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bottle::Application.config.secret_token = '4eaa2b78858c16d395f742d705b4cbc048518888fe5e9e68a953a38c3df12a4f23060703379bb2316fe21c8d8d293f7e08fbb78aba8d57d14fd5a22da219ea9b'
